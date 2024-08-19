742ae78680f57e5dc33638b2fbb1b40beab77321
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2024-08-19 09:05:30 +0100

    Set up CORS

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const db = require("./db/db");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.post("/items", (req, res) => {
